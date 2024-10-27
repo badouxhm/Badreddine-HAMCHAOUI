3826f486bca0712a449ba346842523ce6e7fe067
HAMCHAOUI Badreddine <badouhm2003@gmail.com>
2024-10-27 21:10:00 +0100

    1

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://Badreddine-HAMCHAOUI.github.io',
+  base: 'Badreddine-HAMCHAOUI',
+});
